3c3f986eeb0e233514c0ca514c18ea9c52c3b2b5
Erik Svedäng <erik.svedang@gmail.com>
2018-03-21 12:42:06 +0100

    ZOMG it works.

--- src/FixedPoint.hs
+++ src/FixedPoint.hs
@@ -1,9 +1,13 @@
+{-# LANGUAGE DeriveFunctor #-}
+
 module FixedPoint where
 
+-- | A simple tree structure where the recursion points are represented by the type variable 'r'
 data TreeF r = Leaf Int
              | Branch r r
-             deriving Show
+             deriving (Show, Eq, Functor)
 
+-- | The type of these becomes longer and longer...
 ex1 :: TreeF r
 ex1 = Leaf 1
 
@@ -15,20 +19,26 @@ ex3 = Branch (Branch (Leaf 10) (Leaf 20)) (Leaf 30)
 
 data Fix f = Fx (f (Fix f))
 
+-- | A way of getting something out of 'Fx'
+outF :: Fix f -> f (Fix f)
 outF (Fx x) = x
 
+-- | The fixed point of TreeF
 type Tree = Fix TreeF
 
+-- | The type of these stay the same! Need to wrap everything in the 'Fx' constructor tho.
 ex4 :: Tree
 ex4 = Fx (Branch (Fx $ Leaf 1) (Fx $ Leaf 2))
 
+ex5 :: Tree
+ex5 = Fx (Leaf 1000)
 
-
+-- | An algebra and a catamorphism function:
 type Algebra f a = f a -> a
 cata :: Functor f => Algebra f a -> Fix f -> a
 cata f = f . fmap (cata f) . outF
 
-
-
-instance Functor TreeF where
-  fmap f (Leaf x) = Leaf (f x)
+total :: Tree -> Int
+total = cata phi where
+  phi (Leaf x) = x
+  phi (Branch a b) = a + b
