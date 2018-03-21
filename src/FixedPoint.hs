6d917e3655edcfbc3c656ed35f0716b43ab79bab
Erik Svedäng <erik.svedang@gmail.com>
2018-03-21 07:21:55 +0100

    Gotta go.

--- src/FixedPoint.hs
+++ src/FixedPoint.hs
@@ -15,7 +15,20 @@ ex3 = Branch (Branch (Leaf 10) (Leaf 20)) (Leaf 30)
 
 data Fix f = Fx (f (Fix f))
 
+outF (Fx x) = x
+
 type Tree = Fix TreeF
 
 ex4 :: Tree
 ex4 = Fx (Branch (Fx $ Leaf 1) (Fx $ Leaf 2))
+
+
+
+type Algebra f a = f a -> a
+cata :: Functor f => Algebra f a -> Fix f -> a
+cata f = f . fmap (cata f) . outF
+
+
+
+instance Functor TreeF where
+  fmap f (Leaf x) = Leaf (f x)
